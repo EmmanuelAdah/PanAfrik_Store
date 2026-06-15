ba9bf9cf539f4be629708f0173bd3ecf25e72a46
emmanuel-adah <edogbanya02@gmail.com>
2026-06-16 00:07:15 +0100

    refactor: enable server monitoring

--- utils/monitorServer.js
+++ utils/monitorServer.js
@@ -1,17 +1,17 @@
-// const logger = require('./logger');
-// const axios = require('axios');
-// const HEALTH_CHECK_URL = process.env.HEALTH_CHECK_URL
-//
-//
-// const monitorServer = async () => {
-//     try {
-//         const health = await axios.get(`${HEALTH_CHECK_URL}`)
-//         logger.info(health.data)
-//     } catch (error) {
-//         logger.error('Server is down: ', error.message)
-//     }
-// }
-//
-// setInterval(() =>{
-//     monitorServer()
-// }, 1000 * 60 * 14)
+const logger = require('./logger');
+const axios = require('axios');
+const HEALTH_CHECK_URL = process.env.HEALTH_CHECK_URL
+
+
+const monitorServer = async () => {
+    try {
+        const health = await axios.get(`${HEALTH_CHECK_URL}`)
+        logger.info(health.data)
+    } catch (error) {
+        logger.error('Server is down: ', error.message)
+    }
+}
+
+setInterval(() =>{
+    monitorServer()
+}, 1000 * 60 * 14)
